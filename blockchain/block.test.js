cbe5fcf2d7cbb9ba94ef0b16ec0aa04fbb58675d
mehulbhuradia <mehulbhuradia007@gmail.com>
2022-05-02 00:31:02 +0530

    proof of work done

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,5 +1,4 @@
 const Block = require("./block");
-const { DIFFICULTY } = require('../config');
 
 describe('Block',()=>{
     let data, lastBlock, block;
@@ -19,8 +18,15 @@ describe('Block',()=>{
     });
 
     it('generates hash that matches the difficulty', ()=>{
-        expect(block.hash.substring('0',DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
-        console.log(block.toString());
+        expect(block.hash.substring('0',block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    });
+
+    it('lowers the difficulty for slowly mined bloacks',()=>{
+        expect(Block.adjustDifficulty(block,block.timestamp+360000)).toEqual(block.difficulty-1);
+    });
+
+    it('raises the difficulty for quickly mined bloacks',()=>{
+        expect(Block.adjustDifficulty(block,block.timestamp+1)).toEqual(block.difficulty+1);
     });
 
 });
