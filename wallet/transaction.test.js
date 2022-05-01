16adbc3f2ff79f1798d8f7e1fbe3d4c26b7b29f3
mehulbhuradia <mehulbhuradia007@gmail.com>
2022-05-02 01:26:40 +0530

    transaction

---
+++ wallet/transaction.test.js
@@ -0,0 +1,38 @@
+const Transaction = require('./transaction');
+const Wallet = require('./index');
+
+describe('Transaction', ()=>{
+
+    let transaction,wallet,recipient,amount;
+
+    beforeEach(()=>{
+        wallet=new Wallet();
+        amount=50;
+        recipient='r3c1p13nt';
+        transaction=Transaction.newTransaction(wallet,recipient,amount);
+    });
+
+    it('outputs the `amount` subtracted from the senders wallet balance',()=>{
+        expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+            .toEqual(wallet.balance - amount);
+    });
+    
+    it('outputs the `amount` added to the recipients wallet balance',()=>{
+        expect(transaction.outputs.find(output => output.address === recipient).amount)
+            .toEqual(amount);
+    });
+    
+    
+    describe('Transaction amount exceeds senders balance', ()=>{
+    
+        beforeEach(()=>{
+            amount=500000;
+            transaction=Transaction.newTransaction(wallet,recipient,amount);
+        });
+        
+          it('does not create the tansaction',()=>{
+            expect(transaction).toEqual(undefined);
+        });
+        
+    });
+});
