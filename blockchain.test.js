992ac8d00ea2568c05d73359dc173f70dee7c66e
mehulbhuradia <mehulbhuradia007@gmail.com>
2022-05-01 22:35:54 +0530

    replace chain

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,11 @@ const Block = require("./block");
 const Blockchain = require("./blockchain");
 
 describe('Blockchain', ()=>{
-    let bc;
+    let bc,bc2;
 
     beforeEach(()=>{
         bc=new Blockchain();
+        bc2=new Blockchain();
     });
 
     it('starts with genesis block',()=>{
@@ -17,5 +18,37 @@ describe('Blockchain', ()=>{
         bc.addBlock(data);
         expect(bc.chain[bc.chain.length-1].data).toEqual(data);
     });
+
+    it('validates a valid chain',()=>{
+        bc2.addBlock('foo');
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('invalidates a chain with a corrupt genesis block',()=>{
+        bc2.chain[0].data='bad data';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('invalidates a corrupt chain',()=>{
+        bc2.addBlock('foo');
+        bc2.chain[1].data = 'not foo';
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('replaces the chain with a valid chain',()=>{
+        bc2.addBlock('foo');
+        bc.replaceChain(bc2.chain)
+        
+        expect(bc.chain).toEqual(bc2.chain);
+    });
+
+    it('does not replace chain with a shorter chain',()=>{
+        bc.addBlock('foo');
+        bc.replaceChain(bc2.chain)
+        
+        expect(bc.chain).not.toEqual(bc2.chain);
+    });
+
 
 });
